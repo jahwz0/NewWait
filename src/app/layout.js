fa99ace17fcf732070ee6fb3fee907b08fa0589c
jahwz0 <jawsjavaughnwhite@gmail.com>
2026-05-17 16:25:13 +0100

    remove silly changes

--- src/app/layout.js
+++ src/app/layout.js
@@ -23,11 +23,6 @@ export const metadata = {
   description: "",
 };
 
-export const viewport = {
-  width: "device-width",
-  initialScale: 1,
-};
-
 export default function RootLayout({ children }) {
   return (
     <html
